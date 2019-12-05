6f9025b539b9546da50c851b64df777f460100f7
Andrew Garr <chico2995@gmail.com>
2019-12-05 15:55:36 +0800

    Added quick_picks

---
+++ Practical 4/quick_picks.py
@@ -0,0 +1,24 @@
+import random
+
+numberlist = []
+select = int(input("number>>> "))
+MAX = 45
+MIN = 1
+number = 0
+
+for i in range(select):
+    for j in range(1,7):
+        number = random.randint(MIN,MAX)
+        for item in numberlist:
+            while(number == item):
+                number = random.randint(MIN,MAX)
+        numberlist.append(number)
+    numberlist.sort()
+    print(numberlist)
+    numberlist = []
+
+
+
+
+
+
