d90b753bfaef2680e31574d12a8d8d289fbd4b78
Andrew Garr <chico2995@gmail.com>
2019-12-19 17:53:05 +0800

    final edit for comments in languages.py and programming_language.py

--- Practical6/languages.py
+++ Practical6/languages.py
@@ -1,16 +1,17 @@
 from Practical6.programming_language import ProgrammingLanguage
 
-planguages = []
+p_languages = []
 
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-planguages.append(ruby)
+p_languages.append(ruby)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-planguages.append(python)
+p_languages.append(python)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-planguages.append(visual_basic)
+p_languages.append(visual_basic)
 
 print("The dynamically typed languages are: ")
-for item in planguages:  # looping through programming languages list
+for item in p_languages:
+    """ looping through programming languages list"""
     answer = item.is_dynamic()
     if answer:
         print(item.get_name())
--- Practical6/programming_language.py
+++ Practical6/programming_language.py
@@ -1,20 +1,19 @@
 class ProgrammingLanguage:
 
     def __init__(self, name, typing, reflection, year):
-        # Construct a ProgrammingLanguage object from given values
+        """Construct a ProgrammingLanguage object from given values"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
-        # Return string representation from ProgrammingLanguage object
+        """Return string representation from ProgrammingLanguage object"""
         return "{}, {} Typing, {}, {}".format(self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
-        # Function to check if programming language is dynamically typed
-        return self.typing == 'Dynamic'
+        """Function to check if programming language is dynamically typed"""
 
-    def get_name(self):  # gets name
-        # function to get the name of the programming language from the programming language object
+    def get_name(self):
+        """Function to get the name of the programming language from the programming language object"""
         return self.name
